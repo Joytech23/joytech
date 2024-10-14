91a2dc2e2b4c9ca4bac7bc2d052a8ac1a01ff6d2
Joytech23 <niteshjoytech@gmail.com>
2024-10-14 12:47:32 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -13,5 +13,5 @@ apt-get update && apt-get install -y \
 playwright install chromium
 
 # Start the Flask app
-gunicorn --bind 0.0.0.0:5000 app:app 
+gunicorn --bind 0.0.0.0:5000 app:app --timeout 120
 
